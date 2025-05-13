c8845cd6fc721995536c21fdb6d14c44df135520
rno <rno@acoss.fr>
2025-05-13 13:48:22 +0200

    TP04

--- launchTp.sh
+++ launchTp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd tp$1
-/opt/venv/bin/ansible-playbook tp.yaml -i host
+unbuffer bash -c "/opt/venv/bin/ansible-playbook tp.yaml -i host" --color=auto | tee a.log 
 
 echo "/opt/venv/bin/ansible-galaxy install -r tp3/requirements.yml "
